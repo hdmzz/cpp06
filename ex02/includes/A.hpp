91106c60c190460e26704495e1f9442ba459f210
Hugo Damitzian <hdamitzi@z1r5p1.42lyon.fr>
2023-12-12 11:58:36 +0100

    correction avanrt repush

--- ex02/includes/A.hpp
+++ ex02/includes/A.hpp
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   A.hpp                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hdamitzi <hdamitzi@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: hdamitzi <hdamitzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 11:58:55 by hdamitzi          #+#    #+#             */
-/*   Updated: 2023/12/06 12:00:18 by hdamitzi         ###   ########.fr       */
+/*   Updated: 2023/12/12 11:57:11 by hdamitzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _HPP
+#ifndef A_HPP
 # define A_HPP
 # include "Base.hpp"
 
